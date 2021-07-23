11068d46fa06127fecf1406a432d0ed687b75eb3
laecio12 <laecio12silva@gmail.com>
2021-07-23 19:36:32 -0300

    update user stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -8,6 +8,9 @@ type User = {
   ref: {
     id: string;
   }
+  data: {
+    stripe_customer_id: string;
+  }
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -23,26 +26,35 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         )
       )
     )
+    
 
+    let customerId = user.data.stripe_customer_id
+    if(!customerId) {
+      
+          const stripeCustomer = await stripe.customers.create({
+            email: session.user.email,
+          })
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-    })
+          await fauna.query(
+            q.Update(
+              q.Ref(q.Collection('users'), user.ref.id),
+              {
+                data: { 
+                  stripe_customer_id: stripeCustomer.id,
+                }
+              }
+           )
+          )
+          
+          customerId = stripeCustomer.id;
+
+    }
+
 
    
-    await fauna.query(
-      q.Update(
-        q.Ref(q.Collection('users'), user.ref.id),
-        {
-          data: { 
-            stripe_customer_id: stripeCustomer.id,
-          }
-        }
-      )
-    )
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId, 
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
